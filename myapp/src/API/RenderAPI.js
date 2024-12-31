4714f8eff30edb104a75784e6c14087aa3f5ca2d
Ananthu Lal <110711073+heyananthu@users.noreply.github.com>
2024-12-31 21:33:42 +0530

    Update RenderAPI.js

--- myapp/src/API/RenderAPI.js
+++ myapp/src/API/RenderAPI.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 // Check if the app is running in production or development
-const isProduction = window.location.hostname !== "localhost";
+// const isProduction = window.location.hostname !== "localhost";
 
 // Set baseURL dynamically based on environment
-const API = axios.create({
-  baseURL: isProduction
-    ? "https://chatapp-2025.onrender.com" // Production URL
-    : "http://localhost:5000", // Development URL
-});
+// const API = axios.create({
+//   baseURL: isProduction
+//     ? "https://chatapp-2025.onrender.com" // Production URL
+//     : "http://localhost:5000", // Development URL
+// });
+
+const API = axios.create({ "https://chatapp-2025.onrender.com" });
 
 export default API;

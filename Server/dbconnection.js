c12f21e9a6a24c40822e9252a6dd9162f00f1fe7
ananthu <heyananthulal@gmail.com>
2025-01-03 20:17:50 +0530

    profilepage done

--- Server/dbconnection.js
+++ Server/dbconnection.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://127.0.0.1/ChatApp")
+// mongoose.connect("mongodb://127.0.0.1/ChatApp")
 
-// mongoose.connect("mongodb+srv://heyananthu:heyananthu@cluster0.ncnpu.mongodb.net/Chat-App?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://heyananthu:heyananthu@cluster0.ncnpu.mongodb.net/Chat-App?retryWrites=true&w=majority&appName=Cluster0")
 
 
 var db = mongoose.connection
